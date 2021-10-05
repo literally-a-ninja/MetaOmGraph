c4fea5e5888c7cffc336a5894d9450d045cb4680
Austin Sickels <sickels0@iastate.edu>
2021-10-05 17:26:37 -0500

    Range filter working
    q

--- src/edu/iastate/metnet/metaomgraph/FilterableTableModel.java
+++ src/edu/iastate/metnet/metaomgraph/FilterableTableModel.java
@@ -171,6 +171,24 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 		return hits;
 	}
 
+	public TreeSet<Integer> applyRangeFilter(Double min, Double max) {
+		TreeSet<Integer> hits = new TreeSet();
+		for (int row = 0; row < model.getRowCount(); row++) {
+			for (int col = 0; col < model.getColumnCount(); col++) {
+				String thisValue = model.getValueAt(row, col) + "";
+				try {
+					Double curr = Double.parseDouble(thisValue);
+					if (curr >= min && curr <= max) {
+						hits.add(Integer.valueOf(row));
+					}
+				} catch (NumberFormatException e) {
+					continue;
+				}
+			}
+		}
+		return hits;
+	}
+
 	/**
 	 * @author urmi modified urmi add columns to query for better searching
 	 * @param values
@@ -180,6 +198,7 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 		// query contating column info is like "searchterm":::"colnumber" where
 		// colnumber starts from zero
 		String delim = ":::";
+
 		// if query ends with --C: means case sensitive
 		String caseFlag = "--C";
 		caseSensitive = false;
@@ -189,6 +208,13 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 				boolean isNotFlag = false;
 				boolean doesNotFlag = false;
 				boolean isFlag = false;
+				boolean rangeFlag = false;
+				if (findMe.length() >= 5 && findMe.contains("RANGE")) {
+					rangeFlag = true;
+					findMe = findMe.replace("EXPR", "");
+					findMe = findMe.replace("{", "");
+					findMe = findMe.replace("}", "");
+				}
 				if(findMe.length() > 1) {
 					if(findMe.substring(0, 2).contentEquals("!=")) {
 						isNotFlag = true;
@@ -207,7 +233,14 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 				boolean anyCol = true;
 				// delim should be present and should have values on both sides
 				if (findMe.indexOf(delim) > -1 && findMe.indexOf(delim) < findMe.length() - delim.length()) {
-					String col = findMe.split(delim)[1];
+					String col = "";
+					if (rangeFlag) {
+						col = findMe.split(delim)[0];
+						anyCol = false;
+						allCols = false;
+					} else {
+						col = findMe.split(delim)[1];
+					}
 					if(col.contentEquals("ALL")) {
 						allCols = true;
 						anyCol = false;
@@ -239,6 +272,7 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 				if (colFlag) {
 					// search only specified column
 					findMe = findMe.split(delim)[0];
+
 					// JOptionPane.showMessageDialog(null, "searching:" + findMe + " col:" +
 					// colInt);
 
@@ -246,7 +280,7 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 						caseSensitive = true;
 						findMe = findMe.split(caseFlag)[0];
 					}
-					
+
 					for (int row = 0; row < model.getRowCount(); row++) {
 						// for (int col = 0; col <= colInt; col++) {
 						String thisValue = model.getValueAt(row, colInt) + "";
@@ -266,8 +300,7 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 							if (thisValue.contentEquals(findMe)) {
 								hits.add(Integer.valueOf(row));
 							}
-						}
-						else if(thisValue.contains(findMe)) {
+						} else if(thisValue.contains(findMe)) {
 							hits.add(Integer.valueOf(row));
 						}
 					}
@@ -280,6 +313,10 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 						hits.addAll(applyAnyColFilter(findMe, isNotFlag, doesNotFlag, isFlag));
 					} else if(allCols) {
 						hits.addAll(applyAllColsFilter(findMe, isNotFlag, doesNotFlag, isFlag));
+					} else if (rangeFlag) {
+						Double min = Double.parseDouble(findMe.split(delim)[1]);
+						Double max = Double.parseDouble(findMe.split(delim)[2]);
+						hits.addAll(applyRangeFilter(min, max));
 					}
 				}
 			}
@@ -310,7 +347,6 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 		catch(Exception e) {
 
 		}
-
 	}
 
 	public synchronized void filterToRows(int[] rows) {
@@ -438,14 +474,14 @@ public class FilterableTableModel extends AbstractTableModel implements Document
 			return selectedRows;
 		int[] result = new int[selectedRows.length];
 		for (int x = 0; x < result.length; x++)
-			result[x] = rowMap.get(new Integer(x)).intValue();
+			result[x] = rowMap.get(Integer.valueOf(x));
 		return result;
 	}
 
 	public int getUnfilteredRow(int selectedRow) {
 		if (rowMap == null)
 			return selectedRow;
-		return rowMap.get(new Integer(selectedRow)).intValue();
+		return rowMap.get(Integer.valueOf(selectedRow));
 	}
 
 	public static void main(String[] args) {
