1f79180846560c707889c2542e40cdb1f43a23bf
John Chandara <chandara@iastate.edu>
2021-10-22 21:46:34 -0500

    Quiet building!

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,12 +15,13 @@ if [[ $BUILD_MODE > $MODE_MIN ]]; then
 	download_lib "http://www.java2s.com/Code/JarDownload/l2fprod/l2fprod-common-all.jar.zip" "l2fprod-common-all.jar" \
 	  && mvn install:install-file \
 	  -Dfile="$DIR_PROJECT/lib/l2fprod-common-all.jar" -Dpackaging=jar -DgeneratePom=true \
-	  -DgroupId='com.l2fprod' -DartifactId='l2fprod-common-all' -Dversion='0.1'
+	  -DgroupId='com.l2fprod' -DartifactId='l2fprod-common-all' -Dversion='0.1' >/dev/null
+
 
 	info "Processing CustomBrowserLauncher library..."
 	mvn install:install-file \
 	  -Dfile="$DIR_PROJECT/lib/CustomBrowserLauncher.jar" -Dpackaging=jar -DgeneratePom=true \
-	  -DgroupId='edu.iastate.metnet' -DartifactId='custombrowserlauncher' -Dversion='0.0.1'
+	  -DgroupId='edu.iastate.metnet' -DartifactId='custombrowserlauncher' -Dversion='0.0.1' >/dev/null
 
 	ok "All libraries are OK."
 fi
@@ -29,4 +30,4 @@ fi
 # Let's build the app!
 # -------------------------------
 important "Building app..."
-mvn verify clean package
+mvn verify clean package >/dev/null
