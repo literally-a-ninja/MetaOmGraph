db450771c0d03583b413fa189293811c5469eb12
aorgler <aorgler@iastate.edu>
2021-11-05 18:13:34 -0500

    tweaked ui, added error checking for inputed values

--- src/edu/iastate/metnet/metaomgraph/MetaOmGraphLauncher.java
+++ src/edu/iastate/metnet/metaomgraph/MetaOmGraphLauncher.java
@@ -1,11 +1,9 @@
 package edu.iastate.metnet.metaomgraph;
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -33,7 +31,9 @@ public class MetaOmGraphLauncher implements ActionListener {
     private JScrollPane messagePane;
     private JTextArea messageLog;
     private JButton runButton;
+    private JButton closeButton;
     private JLabel text;
+    private JLabel errorMessage;
 
     //commands
     private final String EXTRA_MEMORY = "extra memory";
@@ -41,18 +41,20 @@ public class MetaOmGraphLauncher implements ActionListener {
     private final String RUN = "run";
     private final String SET_MIN_MEMORY = "set min memory";
     private final String SET_MAX_MEMORY = "set max memory";
+    private final String EXIT = "exit";
 
     //launch variables
     private boolean extraMemory;
     private int minMemory;
     private int maxMemory;
+    private boolean errorExists;
 
     //debugger log on/off
     private boolean loggerOn = false;
 
     private MetaOmGraphLauncher () {
         // build window
-        frame = new JFrame("MetaOmGraph Launcher");
+        frame = new JFrame("MetaOmGraph");
         frame.setLayout(new GridBagLayout());
         try {
             UIManager.setLookAndFeel(new FlatLightLaf());
@@ -62,51 +64,65 @@ public class MetaOmGraphLauncher implements ActionListener {
 
         // gridbag formatting
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weighty = 1;
-        gbc.weightx = 1;
-
-        //logo constraints
-        gbc.gridx = 3; // x pos
-        gbc.gridy = 0; // y pos
-        gbc.gridwidth = 1; // number of grids to fill horizontally
-        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
 
         // logo
+        gbc.gridx = 0; // x pos
+        gbc.gridy = 0; // y pos
+        gbc.weighty = 0;
+        gbc.weightx = 1;
+        gbc.gridwidth = 3; // number of grids to fill horizontally
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.insets = new Insets(0,0,0,10);
         try {
+            int width = 120;
+            int height = 120;
             BufferedImage image = ImageIO.read(this.getClass().getResourceAsStream(logoPath));
-            mogLogo = new JLabel(new ImageIcon(image));
+            Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            mogLogo = new JLabel(new ImageIcon(scaledImage));
+            mogLogo.setPreferredSize(new Dimension(width, height));
             frame.add(mogLogo, gbc);
             frame.setIconImage(image);
         } catch (IOException e) {
             e.printStackTrace();
             log(e.getMessage());
         }
+
         // Logo Text
         text = new JLabel("MetaOmGraph");
-        gbc.gridy++;
-        gbc.gridx = 3;
-        gbc.gridwidth = 2;
+        text.setFont(new Font("Serif", Font.BOLD, 24));
+        gbc.weighty = 1;
+        gbc.weightx = 1;
+        gbc.gridx = 4;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0,10,0,0);
         frame.add(text, gbc);
 
         // default memory radio button
-        defaultMemoryRadio = new JRadioButton("Use Default Memory");
+        defaultMemoryRadio = new JRadioButton("Open with Default Memory");
         defaultMemoryRadio.setActionCommand(DEFAULT_MEMORY);
         defaultMemoryRadio.addActionListener(this);
+        gbc.weighty = 0.2;
+        gbc.weightx = 1;
         gbc.gridy++;
         gbc.gridx = 0;
         gbc.gridwidth = 7;
         gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0, 30, 0, 0);
         frame.add(defaultMemoryRadio, gbc);
 
         // extra memory radio button
-        extraMemoryRadio = new JRadioButton("Use Extra Memory");
+        extraMemoryRadio = new JRadioButton("Open with Extra Memory");
         extraMemoryRadio.setActionCommand(EXTRA_MEMORY);
         extraMemoryRadio.addActionListener(this);
+        gbc.weighty = 0.2;
+        gbc.weightx = 1;
         gbc.gridy++;
         gbc.gridx = 0;
         gbc.gridwidth = 7;
         gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0, 30, 20, 0);
         frame.add(extraMemoryRadio, gbc);
 
         // group buttons
@@ -116,43 +132,89 @@ public class MetaOmGraphLauncher implements ActionListener {
 
         // min label
         text = new JLabel("Min:");
+        gbc.weighty = 0.1;
+        gbc.weightx = 1;
         gbc.gridy++;
         gbc.gridx = 0;
         gbc.gridwidth = 1;
-        gbc.anchor = GridBagConstraints.EAST;
+        gbc.anchor = GridBagConstraints.NORTHEAST;
+        gbc.insets = new Insets(0,0,0,0);
         frame.add(text, gbc);
         // min memory
         minMemoryBox = new JTextField(5);
         minMemoryBox.setHorizontalAlignment(JTextField.CENTER);
         minMemoryBox.setActionCommand(SET_MIN_MEMORY);
         minMemoryBox.addActionListener(this);
+        gbc.weightx = 0;
         gbc.gridx++;
-        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.anchor = GridBagConstraints.NORTH;
         frame.add(minMemoryBox, gbc);
         // min label GB
         text = new JLabel("GB");
+        gbc.weightx = 1;
         gbc.gridx++;
-        gbc.anchor = GridBagConstraints.WEST;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         frame.add(text, gbc);
 
         // max label
         text = new JLabel("Max:");
+        gbc.weighty = 0.1;
+        gbc.weightx = 1;
         gbc.gridx = 4;
-        gbc.anchor = GridBagConstraints.EAST;
+        gbc.anchor = GridBagConstraints.NORTHEAST;
+        gbc.insets = new Insets(0,0,0,0);
         frame.add(text, gbc);
         // max memory
         maxMemoryBox = new JTextField(5);
         maxMemoryBox.setHorizontalAlignment(JTextField.CENTER);
         maxMemoryBox.setActionCommand(SET_MAX_MEMORY);
         maxMemoryBox.addActionListener(this);
+        gbc.weightx = 0;
         gbc.gridx++;
-        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.anchor = GridBagConstraints.NORTH;
         frame.add(maxMemoryBox, gbc);
-        // mac label GB
+        // max label GB
         text = new JLabel("GB");
         gbc.gridx++;
-        gbc.anchor = GridBagConstraints.EAST;
+        gbc.weightx = 1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         frame.add(text, gbc);
+
+        errorMessage = new JLabel(" ");
+        errorMessage.setForeground(Color.RED);
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 7;
+        gbc.weighty = 0.1;
+        gbc.weightx = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0,0,0,0);
+        frame.add(errorMessage, gbc);
+
+        // close program button
+        closeButton = new JButton("Close");
+        closeButton.setActionCommand(EXIT);
+        closeButton.addActionListener(this);
+        gbc.gridy++;
+        gbc.gridx = 2;
+        gbc.gridwidth = 1;
+        gbc.weighty = 0.3;
+        gbc.weightx = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.insets = new Insets(30,0,0,0);
+        frame.add(closeButton, gbc);
+
+        // run program button
+        runButton = new JButton("Next");
+        runButton.setActionCommand(RUN);
+        runButton.addActionListener(this);
+        gbc.gridx = 4;
+        gbc.gridwidth = 1;
+        gbc.weighty = 0.3;
+        gbc.weightx = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.insets = new Insets(30,0,0,0);
+        frame.add(runButton, gbc);
 
         // message log
         if (loggerOn) {
@@ -161,20 +223,14 @@ public class MetaOmGraphLauncher implements ActionListener {
             messagePane = new JScrollPane(messageLog);
             gbc.gridx = 0;
             gbc.gridy++;
-            gbc.gridwidth = 6;
+            gbc.gridwidth = 7;
+            gbc.weighty = 0;
+            gbc.weightx = 0;
             gbc.anchor = GridBagConstraints.CENTER;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.insets = new Insets(0,0,0,0);
             frame.add(messagePane, gbc);
         }
-
-        // run program button
-        runButton = new JButton("Run");
-        runButton.setActionCommand(RUN);
-        runButton.addActionListener(this);
-        gbc.gridy++;
-        gbc.gridx = 3;
-        gbc.gridwidth = 1;
-        gbc.anchor = GridBagConstraints.CENTER;
-        frame.add(runButton, gbc);
 
         // init launch variables
         initVariables();
@@ -185,23 +241,26 @@ public class MetaOmGraphLauncher implements ActionListener {
 
     private void initVariables() {
         extraMemory = false;
-        minMemory = 0;
-        maxMemory = 0;
+        minMemory = 2;
+        maxMemory = 6;
         defaultMemoryRadio.setSelected(true);
         defaultMemory();
+        voidErrors();
     }
 
     // start the program
     private void init() {
-        frame.setSize(400, 300);
+        frame.setSize(600, 450);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String com = e.getActionCommand();
-
+        voidErrors();
         switch (com) {
             case DEFAULT_MEMORY:
             {
@@ -224,6 +283,9 @@ public class MetaOmGraphLauncher implements ActionListener {
             case SET_MAX_MEMORY: {
                 setMaxMemory();
                 break;
+            }
+            case EXIT: {
+                exit();
             }
             default:
             {
@@ -256,25 +318,49 @@ public class MetaOmGraphLauncher implements ActionListener {
     // needs error checking - aorgler
     private void setMinMemory() {
         String input = minMemoryBox.getText();
+        // wrong input checks:
         for(char c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
+                logError("Input number values only!");
                 return;
             }
         }
-        minMemory = Integer.parseInt(input);
-        log("Set min: " +  minMemory + "gb");
+        int value = Integer.parseInt(input);
+        if (value > 5 || value < 0) {
+            logError("MIN value must be between 0-5GB!");
+            return;
+        } else if (value >= maxMemory) {
+            logError("MIN value cannot be higher than MAX!");
+            return;
+        }
+        minMemory = value;
+        log("Set MIN: " +  minMemory + "GB");
     }
 
-    //
+    // sets max value
     private void setMaxMemory() {
         String input = maxMemoryBox.getText();
         for(char c : input.toCharArray()) {
             if (!Character.isDigit(c)) {
+                logError("Input number values only!");
                 return;
             }
         }
-        maxMemory = Integer.parseInt(input);
-        log("Set min: " +  maxMemory + "gb");
+        int value = Integer.parseInt(input);
+        if (value > 6 || value < 1) {
+            logError("MAX value must be between 1-6GB!");
+            return;
+        } else if (value <= minMemory) {
+            logError("MAX value cannot be lower than MIN!");
+            return;
+        }
+        maxMemory = value;
+        log("Set MAX: " +  maxMemory + "GB");
+    }
+
+    private void exit() {
+        log("exiting...");
+        System.exit(0);
     }
 
     // method which launches MOG
@@ -304,6 +390,12 @@ public class MetaOmGraphLauncher implements ActionListener {
             flags.add(maxHeap);
         }
 
+        // check for input errors before running
+        if (errorExists) {
+            log("Errors exist! Abandoned run!");
+            return;
+        }
+
         for (String f: flags) {
             cmd += f + " ";
         }
@@ -328,6 +420,17 @@ public class MetaOmGraphLauncher implements ActionListener {
             // scroll to the bottom of the pane
             messageLog.setCaretPosition(messageLog.getDocument().getLength());
         }
+    }
+
+    private void logError(String error) {
+        log("ERROR! " + error);
+        this.errorMessage.setText(error);
+        this.errorExists = true;
+    }
+
+    private void voidErrors() {
+        this.errorMessage.setText(" ");
+        this.errorExists = false;
     }
 
     // main method
