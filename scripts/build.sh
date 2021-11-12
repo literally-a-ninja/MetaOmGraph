b4eb0a8afade7958fec3bd770da664fbb25d4a77
John <chandara@iastate.edu>
2021-11-12 13:24:05 -0600

    make sure we're in root dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,7 @@ source "${DIR_BOOT}/libraries.sh"
 
 ## Libraries
 ## ===================== ##
+cd "$DIR_ROOT"
 source "$DIR_SCRIPT/bootstrap_libraries.sh"
 
 ok "Build system is READY, building in three seconds."
