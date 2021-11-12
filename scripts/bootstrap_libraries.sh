96376501f059b912911a64c73599a3ad1b129560
John <chandara@iastate.edu>
2021-11-12 13:42:48 -0600

    removed local zip files disguised as jar files

--- scripts/bootstrap_libraries.sh
+++ scripts/bootstrap_libraries.sh
@@ -10,17 +10,4 @@ important "Checking libraries..."
 
 pip3 install -r "$DIR_SCRIPT/build/mvnp/requirements" &>/dev/null
 
-important "Processing l2fprod-common library..." \
-[[ ! -f "$DIR_PROJECT/lib/l2fprod-common-all.jar" ]] \
-  && download_libz "http://www.java2s.com/Code/JarDownload/l2fprod/l2fprod-common-all.jar.zip" "l2fprod-common-all.jar"
-mvn install:install-file -Dfile="$DIR_PROJECT/lib/l2fprod-common-all.jar" -DgroupId='com.l2fprod' -DartifactId='l2fprod-common-all' -Dversion='0.1' -Dpackaging=jar -DgeneratePom=true 2>&1
-
-important "Processing CustomBrowserLauncher library..."
-
-mvn install:install-file -Dfile="$DIR_PROJECT/lib/CustomBrowserLauncher.jar" -DgroupId='edu.iastate.metnet' -DartifactId='custombrowserlauncher' -Dversion='0.0.1' -Dpackaging=jar -DgeneratePom=true 2>&1
-
-important "Processing Hierarchial Clustering library..."
-[[ ! -f "$DIR_PROJECT/lib/hierarchical-clustering-1.2.0.jar" ]] \
-  && download_lib "https://github.com/lbehnke/hierarchical-clustering-java/releases/download/v1.2.0/hierarchical-clustering-1.2.0.jar" "hierarchical-clustering-1.2.0.jar"
-
-mvn install:install-file -Dfile="$DIR_PROJECT/lib/hierarchical-clustering-1.2.0.jar" -DgroupId='com.apporiented' -DartifactId='hierarchical-clustering' -Dversion='1.2.0' -Dpackaging=jar -DgeneratePom=true 2>&1 
+mvn install:install-file -Dfile="$DIR_PROJECT/lib/BrowserLauncher2-all-1_3.jar" -DgroupId='edu.iastate.metnet' -DartifactId='custombrowserlauncher' -Dversion='0.0.1' -Dpackaging=jar -DgeneratePom=true 2>&1
