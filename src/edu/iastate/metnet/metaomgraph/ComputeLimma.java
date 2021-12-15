98d5aaf7ae223e5c600577faaeec2fa24100b77e
Fahmi Rafie <fahmi@iastate.edu>
2021-12-15 14:13:01 -0600

    Working voom plot & toptable

--- src/edu/iastate/metnet/metaomgraph/ComputeLimma.java
+++ src/edu/iastate/metnet/metaomgraph/ComputeLimma.java
@@ -42,7 +42,6 @@ public class ComputeLimma {
         String[] rowHeaders = new String[selected.length];
         String[] columnHeaders = new String[limmaCountsInd.size()];
 
-        DoubleMatrixBuilder rData = new DoubleMatrixBuilder(limmaCountsInd.size(), selected.length);
         StringMatrixBuilder rGroups = new StringMatrixBuilder(2, limmaCountsInd.size());
 
         // create a script engine manager:
@@ -50,24 +49,33 @@ public class ComputeLimma {
         // create a Renjin engine:
         ScriptEngine engine = factory.getScriptEngine();
 
-        StringWriter outputWriter = new StringWriter();
-        engine.getContext().setWriter(outputWriter);
+
 
         int[] example = limmaCountsInd.stream().mapToInt(i->i).toArray();
 
         // Start of Limma analysis
         try {
             thisData = myProject.getSelectedListOnlyRowData(example, selectedList);
+
             rowHeaders = myProject.getDefaultRowNames(selected);
             columnHeaders = myProject.getDataColumnHeaders(example);
 
-            for (int row = 0; row < limmaCountsInd.size(); row++) {
-                for (int col = 0; col < selected.length; col++) {
-                    rData.setValue(row, col, thisData[row][col]);
+//            for (int row = 0; row < limmaCountsInd.size(); row++) {
+//                for (int col = 0; col < selected.length; col++) {
+//                    rData.setValue(row, col, thisData[row][col]);
+//                }
+//            }
+//            rData.setColNames(Arrays.asList(rowHeaders));
+//            rData.setRowNames(Arrays.asList(columnHeaders));
+
+            DoubleMatrixBuilder rData = new DoubleMatrixBuilder(selected.length, limmaCountsInd.size());
+            for (int row = 0; row < selected.length; row++) {
+                for (int col = 0; col < limmaCountsInd.size(); col++) {
+                    rData.setValue(row, col, thisData[col][row]);
                 }
             }
-            rData.setColNames(Arrays.asList(rowHeaders));
-            rData.setRowNames(Arrays.asList(columnHeaders));
+            rData.setColNames(Arrays.asList(columnHeaders));
+            rData.setRowNames(Arrays.asList(rowHeaders));
 
             for (int i = 0; i < limmaCountsInd.size(); i++) {
                 rGroups.setValue(0, i, columnHeaders[i]);
@@ -89,43 +97,47 @@ public class ComputeLimma {
             engine.eval("group2 <- interaction(groups[2,])");
 
             /////////////
-            engine.eval("countsx <- read.table(\"/home/fahmi/3032_3_sd6_mog/rscripts/limma_counts.csv\", header = TRUE, sep = \",\")");
-            engine.eval("counts2x <- countsx[,-1]");
-            engine.eval("rownames(counts2x) <- countsx[,1]");
-            engine.eval("countsx <- data.matrix(counts2x)");
-            engine.eval("d0x <- DGEList(countsx)");
-            engine.eval("d0x <- calcNormFactors(d0x)");
-            engine.eval("snamesx <- colnames(countsx) # Sample names");
-            engine.eval("groupsx <- read.table(\"/home/fahmi/3032_3_sd6_mog/rscripts/limma_groups.csv\", header = TRUE, sep = \",\")");
-            engine.eval("groupx <- interaction(groupsx['Groups'])");
-
+//            engine.eval("countsx <- read.table(\"/home/fahmi/3032_3_sd6_mog/rscripts/limma_counts.csv\", header = TRUE, sep = \",\")");
+//            engine.eval("counts2x <- countsx[,-1]");
+//            engine.eval("rownames(counts2x) <- countsx[,1]");
+//            engine.eval("countsx <- data.matrix(counts2x)");
+//            engine.eval("d0x <- DGEList(countsx)");
+//            engine.eval("d0x <- calcNormFactors(d0x)");
+//            engine.eval("snamesx <- colnames(countsx) # Sample names");
+//            engine.eval("groupsx <- read.table(\"/home/fahmi/3032_3_sd6_mog/rscripts/limma_groups.csv\", header = TRUE, sep = \",\")");
+//            engine.eval("groupx <- interaction(groupsx['Groups'])");
 
             /////////////
 
+            String currentPath = System.getProperty("user.home");
+            String pngPath = "png(\"" + currentPath + "/metaomgraph/voom.png\")";
+            System.out.println(pngPath);
             engine.eval("mm <- model.matrix(~0 + group2)");
-            engine.eval("y <- voom(d, mm, plot = F)");
+            engine.eval(pngPath);
+            engine.eval("y <- voom(d, mm, plot = T)");
+            engine.eval("dev.off()");
 
             engine.eval("fit <- lmFit(y, mm)");
-            engine.eval("contr <- makeContrasts(groupgroup1 - groupgroup2, levels = colnames(coef(fit)))");
+            engine.eval("fitbit <- colnames(coef(fit))");
+
+            engine.eval("contr <- makeContrasts(group21 - group22, levels = colnames(coef(fit)))");
 
             engine.eval("tmp <- contrasts.fit(fit, contr)");
             engine.eval("tmp <- eBayes(tmp)");
 
-            engine.eval("top.table <- topTable(tmp, coeff=1, sort.by = \"P\", n = Inf)");
+            engine.eval("top.table <- topTable(tmp, coef=1, sort.by = \"P\", n = Inf)");
             engine.eval("top.table$Gene <- rownames(top.table)");
             engine.eval("top.table <- top.table[,c(\"Gene\", names(top.table)[1:6])]");
 
-            String currentPath = System.getProperty("user.dir");
-            String dePath = currentPath + "/rscripts/de.tsv";
-            String writeDETable = "write.table(top.table, file = \"" + dePath + "\", row.names = F, sep = \"\t\", quote = F)";
+            String writeDETable = "write.table(top.table, file = \"" + currentPath + "/metaomgraph/de.tsv" + "\", row.names = F, sep = \"\t\", quote = F)";
             engine.eval(writeDETable);
 
-            StringVector genes = (StringVector)engine.eval("top.table$Gene");
-            DoubleVector logFC = (DoubleVector)engine.eval("top.table$logFC");
-            DoubleVector aveExpr = (DoubleVector)engine.eval("top.table$AveExpr");
-            DoubleVector t = (DoubleVector)engine.eval("top.table$t");
-            DoubleVector pVal = (DoubleVector)engine.eval("top.table$P.Value");
-            DoubleVector adjPVal = (DoubleVector)engine.eval("top.table$adj.P.Val");
+//            StringVector genes = (StringVector)engine.eval("top.table$Gene");
+//            DoubleVector logFC = (DoubleVector)engine.eval("top.table$logFC");
+//            DoubleVector aveExpr = (DoubleVector)engine.eval("top.table$AveExpr");
+//            DoubleVector t = (DoubleVector)engine.eval("top.table$t");
+//            DoubleVector pVal = (DoubleVector)engine.eval("top.table$P.Value");
+//            DoubleVector adjPVal = (DoubleVector)engine.eval("top.table$adj.P.Val");
 
             System.out.println("Done!");
 
